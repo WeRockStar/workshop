b7003fd741adcd1dc61f9eed0a9028216f63f959
WeRockStar <kotchaphan.m@gmail.com>
2018-11-24 15:08:55 +0700

    Add update status topic

--- main.go
+++ main.go
@@ -12,10 +12,27 @@ import (
 
 func main() {
 	e := echo.New()
-	e.Use(middleware.Logger())
 
-	e.POST("/todos", create)
+	session, err := mgo.Dial("root:example@13.250.119.252")
+	if err != nil {
+		e.Logger.Fatal(err)
+		return
+	}
+
+	h := &handler{
+		m: session,
+	}
+
+	e.Use(middleware.Logger())
+	e.GET("/todos", h.list)
+	e.POST("/todos", h.create)
+	e.PUT("/todos/:id", h.done)
+	e.GET("/todos/:id", h.view)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type handler struct {
+	m *mgo.Session
 }
 
 type todo struct {
@@ -24,16 +41,57 @@ type todo struct {
 	Done  bool          `json:"done" bson:"done"`
 }
 
-func create(c echo.Context) error {
+func (h *handler) done(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+
+	id := bson.ObjectIdHex(c.Param("id"))
+	var t todo
+	col := session.DB("workshop").C("todos")
+	if err := col.FindId(id).One(&t); err != nil {
+		return err
+	}
+	t.Done = true
+	if err := col.UpdateId(id, t); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, t)
+}
+
+func (h *handler) view(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+
+	id := bson.ObjectIdHex(c.Param("id"))
+	var t todo
+	col := session.DB("workshop").C("todos")
+	if err := col.FindId(id).One(&t); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, t)
+}
+
+func (h *handler) list(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+	var ts []todo
+	col := session.DB("workshop").C("todos")
+	if err := col.Find(nil).All(&ts); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, ts)
+}
+
+func (h *handler) create(c echo.Context) error {
+	session := h.m.Copy()
+	defer session.Close()
+
 	var t todo
 	if err := c.Bind(&t); err != nil {
 		return err
 	}
-	session, err := mgo.Dial("root:example@13.250.119.252")
-	if err != nil {
-		return err
-	}
 	t.ID = bson.NewObjectId()
+
 	col := session.DB("workshop").C("todos")
 	if err := col.Insert(t); err != nil {
 		return err
