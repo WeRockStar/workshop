66dbaee283d82fd13044a8bf629bd328cbefa875
WeRockStar <kotchaphan.m@gmail.com>
2018-11-24 14:14:01 +0700

    Add bson

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 
+	"github.com/globalsign/mgo/bson"
+
+	"github.com/globalsign/mgo"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -16,14 +19,23 @@ func main() {
 }
 
 type todo struct {
-	ID    string `json:id`
-	Topic string `json:topic`
-	Done  bool   `json:done`
+	ID    bson.ObjectId `json:"id" bson:"_id"`
+	Topic string        `json:"topic" bson:"topic"`
+	Done  bool          `json:"done" bson:"done"`
 }
 
 func create(c echo.Context) error {
 	var t todo
 	if err := c.Bind(&t); err != nil {
+		return err
+	}
+	session, err := mgo.Dial("root:example@13.250.119.252")
+	if err != nil {
+		return err
+	}
+	t.ID = bson.NewObjectId()
+	col := session.DB("workshop").C("todos")
+	if err := col.Insert(t); err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, t)
